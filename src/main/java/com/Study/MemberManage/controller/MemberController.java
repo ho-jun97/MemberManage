db28cfd7c3546e94dd131c6503b5a894bec268f3
Hojun <hojun9707@naver.com>
2022-01-07 21:53:53 +0900

    업데이트

--- src/main/java/com/Study/MemberManage/controller/MemberController.java
+++ src/main/java/com/Study/MemberManage/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -29,13 +30,40 @@ public class MemberController {
     @PostMapping("/member/writedo")
     public String memberWriteDo(Member member){
         memberService.write(member);
-        return "redirect:/";
+        return "redirect:/member/list";
     }
 
     @GetMapping("/member/list")
-    public String memberList(Model model){
+    public String memberList(Model model)throws Exception{
         List<Member> member = memberService.memberList();
+
         model.addAttribute("list",member);
         return "memberlist";
     }
+
+    @GetMapping("/member/view")
+    public String memberView(Model model, Integer id){
+        model.addAttribute("member",memberService.memberView(id));
+        return "memberview";
+    }
+
+    @GetMapping("/member/delete")
+    public String memberDelete(Integer id){
+        memberService.memberDelete(id);
+
+        return "redirect:/member/list";
+    }
+
+    @GetMapping("/member/modify/{id}")
+    public String memberModify(@PathVariable("id")Integer id, Model model){
+        model.addAttribute("member",memberService.memberView(id));
+
+        return "membermodify";
+    }
+
+    @PostMapping("/member/update/{id}")
+    public String memberUpdate(@PathVariable("id")Integer id, Member member){
+        return "";
+    }
+
 }
--- src/main/java/com/Study/MemberManage/service/MemberService.java
+++ src/main/java/com/Study/MemberManage/service/MemberService.java
@@ -4,8 +4,12 @@ import com.Study.MemberManage.entity.Member;
 import com.Study.MemberManage.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -65,7 +69,100 @@ public class MemberService {
         memberRepository.save(member);
     }
 
-    public List<Member> memberList(){
-        return memberRepository.findAll();
+    public List<Member> memberList() throws Exception{
+        List<Member> members = memberRepository.findAll();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate now = LocalDate.now();
+        String toDayDate = now.format(formatter); // 오늘 날짜 String 반환
+
+        // 남은 날짜
+        for(Member member : members) {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            String endDay = member.getEndDay();
+            String today = toDayDate;
+
+            Date endDayDate = dateFormat.parse(endDay);
+            Date todayDate = dateFormat.parse(today);
+
+            int remainDay = (int) ((endDayDate.getTime() - todayDate.getTime()) / (24 * 60 * 60 * 1000));
+
+            if (remainDay < 0) {
+                remainDay = 0;
+            }
+            member.setRemainDay(remainDay);
+        }
+
+        return members;
+    }
+
+    public Member memberView(Integer id){
+        return memberRepository.findById(id).get();
+    }
+
+    public void memberDelete(Integer id){
+        memberRepository.deleteById(id);
+    }
+
+    public Member memberUpdate(Member member, Member update){
+
+        /**
+         * 할인율 셋팅
+         * 브론즈    0%
+         * 실버     5%
+         * 골드     10%
+         * 플래티넘  15%
+         * 다이아   20%
+         */
+
+        String memberGrade = member.getGrade();  // 멤버의 등급을 받아옴
+        double discountPriceDouble = 0; // 멤버로 할인된 가격
+        switch (memberGrade) {
+            case "브론즈" :
+                discountPriceDouble = member.getPay();
+                break;
+            case "실버" :
+                discountPriceDouble = member.getPay() * 0.95;
+                break;
+            case "골드" :
+                discountPriceDouble = member.getPay() * 0.9;
+                break;
+            case "플래티넘" :
+                discountPriceDouble = member.getPay() * 0.85;
+                break;
+            case "다이아" :
+                discountPriceDouble = member.getPay() * 0.8;
+                break;
+        }
+        int discountPrice = (int)discountPriceDouble;
+
+        update.setGrade(member.getGrade());
+        update.setName(member.getName());
+        update.setType(member.getType());
+        update.setPay(discountPrice);
+        update.setAddress(member.getAddress());
+        update.setPhone(member.getPhone());
+
+
+//        // 신청일자 설정
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//        String dateString = format.format(date);
+//        update.setDay(dateString);
+//        // 신청일자 셋팅 완료
+
+        // 신청일자는 수정하면 안됨 (수정하면 수정한 날짜로 변경되기 때문)
+
+
+        // 종료일자 설정
+        Calendar cal = Calendar.getInstance(); // 날짜 계산을 위해 Calendar 추상클래스 선언 및 getInstance() 메서드 사용
+        cal.setTime(date);
+        cal.add(Calendar.DATE, member.getRemainDay());
+        String dateString2 = format.format(cal.getTime());
+        update.setEndDay(dateString2);
+        // 종료일자 셋팅 완료
+
+        return update;
     }
 }
